aa40e0c4a82535864e28206faee8538cd8202b29
Nathan Garside <git@nathangarside.com>
2023-11-11 19:54:03 +0000

    wip

--- distrobox/entrypoint.sh
+++ distrobox/entrypoint.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
+
 # This is free and unencumbered software released into the public domain.
 
 # Install non-free packages
-sudo zypper --non-interactive install code
-
-# Fallback to default entrypoint
-/bin/bash -c "$@"
+zypper --non-interactive install code
