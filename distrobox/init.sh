8b7ac0c4248409fd3e009e7186f34d12d0ebb0d5
Nathan Garside <git@nathangarside.com>
2023-11-12 07:43:42 +0000

    Add VS config files to distrobox

--- distrobox/init.sh
+++ distrobox/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # This is free and unencumbered software released into the public domain.
 
-# Install non-free packages
+# Change directoy ownership.
+# Cannot be done in containerfile as user/group is not known at build time.
+chown --recursive nathan:nathan /opt/code
+
+# Install non-free packages.
 zypper --non-interactive install code
