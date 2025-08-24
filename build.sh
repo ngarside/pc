cc7b53e9cb65816dbf0b1c87f0cad34cb8a80753
Nathan Garside <git@nathangarside.com>
2025-08-24 16:21:56 +0100

    use docker format

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-podman build --file containerfile --progress plain --tag ghcr.io/ngarside/pc:latest .
+podman build --file containerfile --format docker --progress plain --tag ghcr.io/ngarside/pc:latest .
