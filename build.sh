6b184b1eab47ba1ac4419ca3da8d26be1e93c93f
Nathan Garside <git@nathangarside.com>
2025-08-24 16:25:26 +0100

    fix selinux

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-podman build --file containerfile --format docker --progress plain --tag ghcr.io/ngarside/pc:latest .
+podman build --file containerfile --format docker --progress plain --security-opt label=disable --tag ghcr.io/ngarside/pc:latest .
