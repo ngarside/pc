12cca5a7b7cfed7417506dd970ea08fd0e97d4bf
Nathan Garside <git@nathangarside.com>
2025-08-24 20:39:55 +0100

    Create 08-install.sh

---
+++ src/08-install.sh
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+rpm-ostree install \
+    dotnet-sdk-9.0 \
+    install \
+    just \
+    godot \
+    golang \
+    zig
