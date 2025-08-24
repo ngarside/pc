c274b60c2a3da7cf00417e93245d8fb3660df1bc
Nathan Garside <git@nathangarside.com>
2025-08-24 21:19:02 +0100

    a

--- silverblue/02-background.sh
+++ silverblue/02-background.sh
@@ -1,3 +1,5 @@
 # This is free and unencumbered software released into the public domain.
 
+mkdir /usr/share/backgrounds/custom
+
 cp /tmp/git/mountains.png /usr/share/backgrounds/custom/mountains.png
